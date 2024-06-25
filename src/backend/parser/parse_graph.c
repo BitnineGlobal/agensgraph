b366e4d13cfd77ff1d921b3ed1bc78c56f7df4f9
Muhammad Taha Naveed <m.taha.naveed27@gmail.com>
2024-06-25 19:07:23 +0500

    chore: Resolve warnings in the build
    - Added fall through comments where expected to silence compiler
      warnings
    - Added missing break statements in switch cases
    - Fixed unused variable warnings
    - Additionally, this PR removes silent flag from the regression
      test workflow to see the build logs

--- src/backend/parser/parse_graph.c
+++ src/backend/parser/parse_graph.c
@@ -5301,7 +5301,7 @@ extractVerticesExpr(ParseState *pstate, List *exprlist, ParseExprKind exprKind)
 
 			case GRAPHPATHOID:
 				elem = getExprField((Expr *) elem, AG_PATH_VERTICES);
-				/* no break */
+				/* fall through */
 			case VERTEXOID:
 			{
 				GraphDelElem *gde = makeNode(GraphDelElem);
@@ -5341,7 +5341,7 @@ extractEdgesExpr(ParseState *pstate, List *exprlist, ParseExprKind exprKind)
 
 			case GRAPHPATHOID:
 				elem = getExprField((Expr *) elem, AG_PATH_EDGES);
-				/* no break */
+				/* fall through */
 			case EDGEOID:
 			{
 				GraphDelElem *gde = makeNode(GraphDelElem);
